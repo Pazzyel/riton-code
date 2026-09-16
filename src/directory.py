ed0bd8096cebfe9b07957f0a89e26250cae4e4b5
pazzyel <huangjin6380@gmail.com>
2026-09-16 17:49:40 +0800

    refractor: change the confg dir

--- src/directory.py
+++ src/directory.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 
 WORKDIR: Path = Path.cwd()
-SKILL_DIR: Path = WORKDIR / "skills"
-TRANSCRIPT_DIR: Path = WORKDIR / ".transcripts"
-TOOL_RESULTS_DIR: Path = WORKDIR / ".task_outputs" / "tool-results"
-MEMORY_DIR: Path = WORKDIR / ".memory"
+CONFIG_DIR: Path = WORKDIR / ".riton"
+SKILL_DIR: Path = CONFIG_DIR / "skills"
+TRANSCRIPT_DIR: Path = CONFIG_DIR / ".transcripts"
+TOOL_RESULTS_DIR: Path = CONFIG_DIR / ".task_outputs" / "tool-results"
+MEMORY_DIR: Path = CONFIG_DIR / ".memory"
 MEMORY_INDEX: Path = MEMORY_DIR / "MEMORY.md"
 USER_AGENTS_MD: Path = Path.home() / ".riton" / "AGENTS.md"
-PROJECT_AGENTS_MD: Path = WORKDIR / "AGENTS.md"
-TASKS_DIR: Path = WORKDIR / ".tasks"
-# RUNTIME_DIR: Path = WORKDIR / ".runtime-tasks"
-DURABLE_PATH = WORKDIR / ".scheduled_tasks.json"
+PROJECT_AGENTS_MD: Path = CONFIG_DIR / "AGENTS.md"
+TASKS_DIR: Path = CONFIG_DIR / ".tasks"
+# RUNTIME_DIR: Path = CONFIG_DIR / ".runtime-tasks"
+DURABLE_PATH = CONFIG_DIR / ".scheduled_tasks.json"
